f40233b48b2415ed522acf868d05b723cfacce7d
Arnold Korir <144571592+SomeKenyan254@users.noreply.github.com>
2025-05-10 12:43:45 -0600

    created about  &home pages  implemented tab browsing functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import "./styles.css";
-const greeting = "I am the best"
+import home from "./modules/home";
+import about from "./modules/about";
+import menu from "./modules/menu";
 
-console.log(greeting)
+const nav = (() => {
+    home.homeCard();
+  
+    const pageLoad = () => {
+        const buttons = document.querySelectorAll(".buttons");
+        buttons.forEach((button) => {
+            button.addEventListener("click", (e) => {
+                const target = e.target.id;
+                if (target === "home") {
+                    content.textContent = "";
+                    home.homeCard();
+                }
+                else if(target === "about"){
+                    content.textContent = "";
+                    about.aboutCard();
+                    
+                }
+                else if(target === "menu"){
+                    content.textContent = "";
+                    menubar.loadAbout()
+                    
+                }
+            });
+        });
+        
+    }
+    return { pageLoad }
+})();
+nav.pageLoad();
---
+++ src/modules/home.js
@@ -0,0 +1,48 @@
+
+const load = (() => {
+   
+    //function to create a blank div //
+    const createCard =(content,className = "card") =>{
+        const card = document.createElement("div")
+        card.classList.add(className)
+        card.textContent = content;
+    
+        return card
+    }
+
+    const homeCard = () => {
+        const card_mission = "Bite Into Bliss: Where Fresh Meets Flavor!"
+        const para = "Established in 1905 Bliss Burgers has been providing exquisite burgers to the residence of Hoodoo Valley. Welcome and experience the Bliss too!"
+        
+    
+        const content = document.querySelector('#content')
+        
+
+        //create cards//
+        const paraCard = createCard(para,"para")
+        const missionCard = createCard (card_mission,"mission")
+       
+        
+      
+        //add the content to page
+        content.appendChild(missionCard) 
+        content.appendChild(paraCard)
+       
+       
+        
+
+    };
+    
+    return {homeCard,createCard}
+
+    
+
+   
+    
+    
+   
+
+})();
+
+
+export default load
